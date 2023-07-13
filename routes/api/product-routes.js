d6ef182451ae83e046dd3deb535917fbd5d7268b
Francis Reyes <reyesfrancisp@gmail.com>
2023-07-13 01:28:59 -0400

    Added more routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,16 +4,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    // Find all products and include associated Category and Tag data
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    // Find a single product by its `id` and include associated Category and Tag data
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if (!product) {
+      res.status(404).json({ message: 'Product not found' });
+      return;
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -92,8 +114,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    // Delete a product by its `id` value
+    const deletedProduct = await Product.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!deletedProduct) {
+      res.status(404).json({ message: 'Product not found' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 module.exports = router;
