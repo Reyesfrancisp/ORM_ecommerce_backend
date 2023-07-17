cd373954377809a2bcd767d3b667b77acbd1002b
Francis Reyes <reyesfrancisp@gmail.com>
2023-07-17 01:53:12 -0400

    Got it working with insomnia

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
@@ -12,5 +14,6 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
+  console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW)
   console.log(`App listening on port ${PORT}!`);
 });
